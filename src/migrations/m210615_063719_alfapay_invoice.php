91ee2bd182340084366a5054bc458a211eaae71c
akhur <akhur02@mail.ru>
2021-06-15 17:31:20 +0500

    тип поля related_id

--- src/migrations/m210615_063719_alfapay_invoice.php
+++ src/migrations/m210615_063719_alfapay_invoice.php
@@ -14,7 +14,7 @@ class m210615_063719_alfapay_invoice extends Migration
     {
         $this->createTable('alfapay_invoice', [
             'id' => $this->primaryKey(),
-            'related_id' => $this->integer()->null(),
+            'related_id' => $this->string()->null(),
             'related_model' => $this->string()->null(),
             'created_at' => $this->integer(),
             'paid_at' => $this->integer(),
